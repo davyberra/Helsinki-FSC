b9cf7f8ba8fbb30c9a73aeed8462ddf001680577
unknown <davyberra@gmail.com>
2021-04-23 14:51:54 -0700

    finished up to 'old react' in 1.d

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,37 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header.js';
-import Content from './Content.js';
-import Total from './Total.js';
+import Button from './Button.js';
+import React, { useState } from 'react';
 
-function App() {
-  const course = {
-    name: 'Half stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+const History = ({ allClicks }) => {
+  if (allClicks.length == 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {allClicks.join(' ')}
+    </div>
+  )
+}
+
+function App(props) {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
   }
 
   return (
     <div>
-      <Header>{course.name}</Header>
-      <Content part={course.parts[0]} />
-      <Content part={course.parts[1]} />
-      <Content part={course.parts[2]} />
-      <Total parts={course.parts} />
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
     </div>
-  );
+  )
 }
 
-export default App;
+      export default App;
