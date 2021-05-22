1e7c8de484b84bcfd6512643925d8e370dbf2662
unknown <davyberra@gmail.com>
2021-05-21 18:40:03 -0700

    added post method

--- part1/src/App.js
+++ part1/src/App.js
@@ -30,11 +30,13 @@ const App = () => {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        console.log(response)
+      })
   }
 
   const handleNoteChange = (event) => {
@@ -46,6 +48,10 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important)
 
+  const toggleImportanceOf = (id) => {
+    console.log('imortance of ' + id + ' needs to be toggled')
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -56,7 +62,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note =>
-          <Note key={note.id} note={note} important={note.important} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
       <form onSubmit={addNote}>
