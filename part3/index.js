775d4db28088b880fba756014fe0e56e8221d5f1
unknown <davyberra@gmail.com>
2021-05-25 21:05:35 -0700

    finished reading 3.1

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
   {
     id: 1,
@@ -19,6 +21,12 @@ let notes = [
     content: "GET and POST are the most important methods of HTTP protocol",
     date: "2019-05-30T19:20:14.298Z",
     important: true
+  },
+  {
+    id: 4,
+    content: "To delete",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true
   }
 ]
 app.get('/', (request, response) => {
@@ -28,6 +36,53 @@ app.get('/', (request, response) => {
 app.get('/api/notes', (request, response) => {
   response.send(notes)
 })
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+
 
 const PORT = 3001
 app.listen(PORT, () => {
